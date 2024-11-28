7741b900522bcd6ed9e8226a8962bab2386cecec
pinhojuliana <studyjulianaa@gmail.com>
2024-11-28 14:56:20 -0300

    Documentação: Inserindo exemplos nas requisições

--- src/main/java/br/com/julianapinho/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/julianapinho/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,7 +3,15 @@ package br.com.julianapinho.gestao_vagas.modules.company.controllers;
 import br.com.julianapinho.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.julianapinho.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.julianapinho.gestao_vagas.modules.company.use_cases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,7 +30,12 @@ public class JobController {
 
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
-    public ResponseEntity<JobEntity> create(@RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
+    @Tag(name = "Vagas", description = "Informações das vagas")
+    @Operation(summary = "Cadastro de vagas", description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponse(responseCode = "200",
+            content = @Content(schema = @Schema(implementation = JobEntity.class)))
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<JobEntity> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
         Object companyId = request.getAttribute("company_id");
 
         var jobEntity = JobEntity.builder()
--- src/main/java/br/com/julianapinho/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/julianapinho/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,4 +1,17 @@
 package br.com.julianapinho.gestao_vagas.modules.company.dto;
 
-public record CreateJobDTO(String description, String benefits, String level) {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateJobDTO(
+        //conteudo / é obrigatorio ou nao
+        @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank
+        String description,
+        @Schema(example = "GYMPass, Plano de Saúde", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank
+        String benefits,
+        @NotBlank
+        @Schema(example = "Júnior", requiredMode = Schema.RequiredMode.REQUIRED)
+        String level) {
 }
