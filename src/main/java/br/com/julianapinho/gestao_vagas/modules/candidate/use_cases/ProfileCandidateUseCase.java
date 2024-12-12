2517c87105c409ebc0dc7cdf9a6c9c345fc742f8
pinhojuliana <studyjulianaa@gmail.com>
2024-12-12 15:20:57 -0300

    Qualidade de código: Refatorando

--- src/main/java/br/com/julianapinho/gestao_vagas/modules/candidate/use_cases/ProfileCandidateUseCase.java
+++ src/main/java/br/com/julianapinho/gestao_vagas/modules/candidate/use_cases/ProfileCandidateUseCase.java
@@ -1,9 +1,9 @@
 package br.com.julianapinho.gestao_vagas.modules.candidate.use_cases;
 
+import br.com.julianapinho.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.julianapinho.gestao_vagas.modules.candidate.repository.CandidateRepository;
 import br.com.julianapinho.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,7 +16,7 @@ public class ProfileCandidateUseCase {
 
     public ProfileCandidateResponseDTO execute(UUID candidateId){
         var candidate = this.repository.findById(candidateId)
-                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+                .orElseThrow(UserNotFoundException::new);
 
         return new ProfileCandidateResponseDTO(candidate.getName(),
                 candidate.getUsername(),
